ce0cc1f068335f438671825a8e98fbe8eb23aaec
dremin <sam@scj.me>
2021-02-20 22:53:54 -0600

    Fill gaps in Environment.SpecialFolder

--- src/ManagedShell.Interop/NativeMethods.Shell32.cs
+++ src/ManagedShell.Interop/NativeMethods.Shell32.cs
@@ -634,10 +634,30 @@ namespace ManagedShell.Interop
         };
 
         [DllImport(Shell32_DllName, EntryPoint = "#727")]
-        public extern static int SHGetImageList(
+        public static extern int SHGetImageList(
             int iImageList,
             ref Guid riid,
             out IImageList ppv
             );
+
+        [DllImport(Shell32_DllName)]
+        public static extern int SHGetKnownFolderPath(
+            [MarshalAs(UnmanagedType.LPStruct)] Guid rfid, uint dwFlags, IntPtr hToken,
+            out IntPtr ppszPath);
+
+        [Flags]
+        public enum KnownFolderFlags : uint
+        {
+            SimpleIDList = 0x00000100,
+            NotParentRelative = 0x00000200,
+            DefaultPath = 0x00000400,
+            Init = 0x00000800,
+            NoAlias = 0x00001000,
+            DontUnexpand = 0x00002000,
+            DontVerify = 0x00004000,
+            Create = 0x00008000,
+            NoAppcontainerRedirection = 0x00010000,
+            AliasOnly = 0x80000000
+        }
     }
 }
