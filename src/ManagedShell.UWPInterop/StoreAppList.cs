443def9a75d5b179ba87e9caf878e2fccbe68b3b
dremin <sam@scj.me>
2021-04-21 17:01:46 -0500

    Make StoreAppList not publicly modifiable

--- src/ManagedShell.UWPInterop/StoreAppList.cs
+++ src/ManagedShell.UWPInterop/StoreAppList.cs
@@ -1,19 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 
 namespace ManagedShell.UWPInterop
 {
-    public class StoreAppList : List<StoreApp>
+    public class StoreAppList : IEnumerable<StoreApp>
     {
+        private List<StoreApp> _appList = new List<StoreApp>();
+
         public void FetchApps()
         {
-            Clear();
-            AddRange(StoreAppHelper.GetStoreApps());
+            _appList.Clear();
+            _appList.AddRange(StoreAppHelper.GetStoreApps());
         }
 
         public StoreApp GetAppByAumid(string appUserModelId)
         {
             // first attempt to get an app in our list already
-            foreach (var storeApp in this)
+            foreach (var storeApp in _appList)
             {
                 if (storeApp.AppUserModelId == appUserModelId)
                 {
@@ -26,12 +29,22 @@ namespace ManagedShell.UWPInterop
 
             if (app != null)
             {
-                Add(app);
+                _appList.Add(app);
                 return app;
             }
 
             // no app found for given AUMID
             return null;
+        }
+
+        public IEnumerator<StoreApp> GetEnumerator()
+        {
+            return ((IEnumerable<StoreApp>)_appList).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)_appList).GetEnumerator();
         }
     }
 }
