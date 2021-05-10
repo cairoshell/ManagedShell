01f54989ca27aaf47fc288c627691a5822f6bd27
Babuki <kevin.boss@outlook.com>
2021-05-10 09:55:11 +0200

    Comparison by device name instead of using equality overrides

--- src/ManagedShell.AppBar/ScreenInfo.cs
+++ src/ManagedShell.AppBar/ScreenInfo.cs
@@ -26,23 +26,5 @@ namespace ManagedShell.AppBar
 		public Rectangle Bounds { get; }
 
 		public bool IsVirtualScreen => DeviceName == nameof(SystemInformation.VirtualScreen);
-
-		protected bool Equals(ScreenInfo other)
-		{
-			return DeviceName == other.DeviceName;
-		}
-
-		public override bool Equals(object obj)
-		{
-			if (ReferenceEquals(null, obj)) return false;
-			if (ReferenceEquals(this, obj)) return true;
-			if (obj.GetType() != this.GetType()) return false;
-			return Equals((ScreenInfo) obj);
-		}
-
-		public override int GetHashCode()
-		{
-			return (DeviceName != null ? DeviceName.GetHashCode() : 0);
-		}
 	}
 }
