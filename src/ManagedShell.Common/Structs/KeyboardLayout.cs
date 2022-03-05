bbe807a34c0a828695f4ab14f04490986eeb8995
Matt <me@curoviyx.ru>
2022-03-06 00:54:13 +0300

    Add KeyboardLayout, rename LayoutHelper...
    ...rename LayoutHelper to KeyboardLayoutHelper

---
+++ src/ManagedShell.Common/Structs/KeyboardLayout.cs
@@ -0,0 +1,9 @@
+﻿namespace ManagedShell.Common.Structs
+{
+    public struct KeyboardLayout
+    {
+        public int HKL;
+        public string NativeName;
+        public string ThreeLetterName;
+    }
+}
