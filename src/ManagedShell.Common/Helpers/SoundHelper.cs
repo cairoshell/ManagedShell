b148cf00dc08b687a4498d75bece80323704614c
Amaro Martínez <xoas@airmail.cc>
2022-11-07 01:09:08 -0500

    Update SoundHelper.cs

--- src/ManagedShell.Common/Helpers/SoundHelper.cs
+++ src/ManagedShell.Common/Helpers/SoundHelper.cs
@@ -8,12 +8,21 @@ namespace ManagedShell.Common.Helpers
     public class SoundHelper
     {
         private const string SYSTEM_SOUND_ROOT_KEY = @"AppEvents\Schemes\Apps";
-        private const int SND_FILENAME = 0x00020000;
-        private const int SND_ASYNC = 0x0001;
-        private const long SND_SYSTEM = 0x00200000L;
+
+        [Flags]
+        private enum PlaySoundFlags
+        {
+            SND_ASYNC = 0x00000001,
+            SND_NODEFAULT = 0x00000002,
+            SND_ALIAS = 0x00010000,
+            SND_FILENAME = 0x00020000,
+            SND_SYSTEM = 0x00200000,
+        }
+
+        private const PlaySoundFlags DEFAULT_SYSTEM_SOUND_FLAGS = PlaySoundFlags.SND_ASYNC | PlaySoundFlags.SND_NODEFAULT | PlaySoundFlags.SND_SYSTEM;
 
         [DllImport("winmm.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        private static extern bool PlaySound(string pszSound, IntPtr hmod, uint fdwSound);
+        private static extern bool PlaySound(string pszSound, IntPtr hmod, PlaySoundFlags soundFlags);
 
         /// <summary>
         /// Plays the specified system sound using the audio session for system notification sounds.
@@ -24,30 +33,21 @@ namespace ManagedShell.Common.Helpers
         {
             try
             {
-                using (RegistryKey key = Registry.CurrentUser.OpenSubKey($"{SYSTEM_SOUND_ROOT_KEY}\\{app}\\{name}\\.Current"))
+                using var key = Registry.CurrentUser.OpenSubKey($@"{SYSTEM_SOUND_ROOT_KEY}\{app}\{name}\.Current");
+                if (key == null)
                 {
-                    if (key == null)
-                    {
-                        ShellLogger.Debug($"SoundHelper: Unable to find sound {name} for app {app}");
-                        return false;
-                    }
-
-                    if (key.GetValue(null) is string soundFileName)
-                    {
-                        if (string.IsNullOrEmpty(soundFileName))
-                        {
-                            ShellLogger.Debug($"SoundHelper: Missing file for sound {name} for app {app}");
-                            return false;
-                        }
-
-                        return PlaySound(soundFileName, IntPtr.Zero, (uint)(SND_ASYNC | SND_FILENAME | SND_SYSTEM));
-                    }
-                    else
-                    {
-                        ShellLogger.Debug($"SoundHelper: Missing file for sound {name} for app {app}");
-                        return false;
-                    }
+                    ShellLogger.Error($"SoundHelper: Unable to find sound {name} for app {app}");
+                    return false;
                 }
+
+                var soundFileName = key.GetValue(null) as string;
+                if (string.IsNullOrEmpty(soundFileName))
+                {
+                    ShellLogger.Error($"SoundHelper: Missing file for sound {name} for app {app}");
+                    return false;
+                }
+
+                return PlaySound(soundFileName, IntPtr.Zero, DEFAULT_SYSTEM_SOUND_FLAGS | PlaySoundFlags.SND_FILENAME);
             }
             catch (Exception e)
             {
@@ -57,29 +57,43 @@ namespace ManagedShell.Common.Helpers
         }
 
         /// <summary>
+        /// Plays the specified system sound using the audio session for system notification sounds.
+        /// </summary>
+        /// <param name="alias">The name of the system sound for ".Default" to play.</param>
+        public static void PlaySystemSound(string alias)
+        {
+            try
+            {
+                PlaySound(alias, IntPtr.Zero, DEFAULT_SYSTEM_SOUND_FLAGS | PlaySoundFlags.SND_ALIAS);
+            }
+            catch (Exception e)
+            {
+                ShellLogger.Error($"SoundHelper: Unable to play sound {alias}: {e.Message}");
+            }
+        }
+
+        /// <summary>
         /// Plays the system notification sound.
         /// </summary>
         public static void PlayNotificationSound()
         {
             // System default sound for the classic notification balloon.
-            if (!PlaySystemSound("Explorer", "SystemNotification"))
+            if (PlaySystemSound("Explorer", "SystemNotification")) return;
+            if (EnvironmentHelper.IsWindows8OrBetter)
             {
-                if (EnvironmentHelper.IsWindows8OrBetter)
-                {
-                    // Toast notification sound.
-                    if (!PlaySystemSound(".Default", "Notification.Default"))
-                        PlayXPNotificationSound();
-                }
-                else
-                {
+                // Toast notification sound.
+                if (!PlaySystemSound(".Default", "Notification.Default"))
                     PlayXPNotificationSound();
-                }
+            }
+            else
+            {
+                PlayXPNotificationSound();
             }
         }
 
-        public static bool PlayXPNotificationSound()
+        public static void PlayXPNotificationSound()
         {
-            return PlaySystemSound(".Default", "SystemNotification");
+            PlaySystemSound("SystemNotification");
         }
     }
 }
