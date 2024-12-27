349d748c6be84822b67feeed4cf20342e809daee
Sam Johnson <sam@scj.me>
2024-12-27 00:28:42 -0600

    Add error handling

--- src/ManagedShell.ShellFolders/ShellLinkHelper.cs
+++ src/ManagedShell.ShellFolders/ShellLinkHelper.cs
@@ -82,48 +82,68 @@ namespace ManagedShell.ShellFolders
             IShellLink link = Load(userInputHwnd, filePath);
             string target = "";
 
-            // First, query Windows Installer to see if this is an installed application shortcut
-            StringBuilder product = new StringBuilder(39);
-            StringBuilder feature = new StringBuilder(39);
-            StringBuilder component = new StringBuilder(39);
-
-            uint result = NativeMethods.MsiGetShortcutTarget(filePath, product, feature, component);
-
-            if (result == 0)
+            try
             {
-                // This is a Windows Installer shortcut
-                int pathLength = 1024;
-                StringBuilder path = new StringBuilder(pathLength);
-                int installState = NativeMethods.MsiGetComponentPath(product.ToString(), component.ToString(), path, ref pathLength);
-                if (installState == 1)
+                // First, query Windows Installer to see if this is an installed application shortcut
+                StringBuilder product = new StringBuilder(39);
+                StringBuilder feature = new StringBuilder(39);
+                StringBuilder component = new StringBuilder(39);
+
+                uint result = NativeMethods.MsiGetShortcutTarget(filePath, product, feature, component);
+
+                if (result == 0)
                 {
-                    // Locally installed application
-                    target = path.ToString();
+                    // This is a Windows Installer shortcut
+                    int pathLength = 1024;
+                    StringBuilder path = new StringBuilder(pathLength);
+                    int installState = NativeMethods.MsiGetComponentPath(product.ToString(), component.ToString(), path, ref pathLength);
+                    if (installState == 1)
+                    {
+                        // Locally installed application
+                        target = path.ToString();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                ShellLogger.Error($"ShellLinkHelper: Unable to query Windows Installer target for {filePath}", e);
+            }
+
+            if (string.IsNullOrEmpty(target))
+            {
+                try
+                {
+                    // Check for an associated identifier list to get an IShellItem object from
+                    IntPtr pidl = IntPtr.Zero;
+                    link.GetIDList(out pidl);
+
+                    if (pidl != IntPtr.Zero)
+                    {
+                        IShellItem _shellItem;
+                        Interop.SHCreateItemFromIDList(pidl, typeof(IShellItem).GUID, out _shellItem);
+                        ShellItem item = new ShellItem(_shellItem);
+                        target = item.Path;
+                    }
+                }
+                catch (Exception e)
+                {
+                    ShellLogger.Error($"ShellLinkHelper: Unable to get ID list for {filePath}", e);
                 }
             }
 
             if (string.IsNullOrEmpty(target))
             {
-                // Check for an associated identifier list to get an IShellItem object from
-                IntPtr pidl = IntPtr.Zero;
-                link.GetIDList(out pidl);
-
-                if (pidl != IntPtr.Zero)
+                try
                 {
-                    IShellItem _shellItem;
-                    Interop.SHCreateItemFromIDList(pidl, typeof(IShellItem).GUID, out _shellItem);
-                    ShellItem item = new ShellItem(_shellItem);
-                    target = item.Path;
-                    string aumid = item.GetAumid();
+                    // Get the shortcut path as a last resort
+                    StringBuilder builder = new StringBuilder(260);
+                    link.GetPath(builder, 260, out Structs.WIN32_FIND_DATA pfd, SLGP_FLAGS.SLGP_RAWPATH);
+                    target = builder.ToString();
                 }
-            }
-
-            if (string.IsNullOrEmpty(target))
-            {
-                // Get the shortcut path as a last resort
-                StringBuilder builder = new StringBuilder(260);
-                link.GetPath(builder, 260, out Structs.WIN32_FIND_DATA pfd, SLGP_FLAGS.SLGP_RAWPATH);
-                target = builder.ToString();
+                catch (Exception e)
+                {
+                    ShellLogger.Error($"ShellLinkHelper: Unable to get path for {filePath}", e);
+                }
             }
 
             return target;
