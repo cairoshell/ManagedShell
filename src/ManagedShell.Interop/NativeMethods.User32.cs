c3871a40e2146d7d6c30ab5b50567f9658380977
Amaro Martínez <xoas@airmail.cc>
2026-08-10 00:10:30 -0500

    Add user32 system menu interop
    Expose the user32 constants and P/Invokes needed to work with window system menus, including cursor position, menu enabling, and popup tracking.

--- src/ManagedShell.Interop/NativeMethods.User32.cs
+++ src/ManagedShell.Interop/NativeMethods.User32.cs
@@ -232,10 +232,36 @@ namespace ManagedShell.Interop
         public const int HSHELL_HIGHBIT = 0x8000;
 
         public const int SC_MINIMIZE = 0xF020;
+        public const int SC_MAXIMIZE = 0xF030;
         public const int SC_MOVE = 0xF010;
         public const int SC_RESTORE = 0xF120;
         public const int SC_SIZE = 0xF000;
         public const int SC_CLOSE = 0xF060;
+
+        public const uint MF_BYCOMMAND = 0x00000000;
+        public const uint MF_GRAYED = 0x00000001;
+        public const uint MF_ENABLED = 0x00000000;
+
+        public const uint TPM_LEFTBUTTON = 0x0000;
+        public const uint TPM_RIGHTBUTTON = 0x0002;
+        public const uint TPM_LEFTALIGN = 0x0000;
+        public const uint TPM_RIGHTALIGN = 0x0008;
+        public const uint TPM_TOPALIGN = 0x0000;
+        public const uint TPM_BOTTOMALIGN = 0x0020;
+        public const uint TPM_VERTICAL = 0x0040;
+        public const uint TPM_RETURNCMD = 0x0100;
+
+        [DllImport(User32_DllName, SetLastError = true)]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+        [DllImport(User32_DllName, SetLastError = true)]
+        public static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+        [DllImport(User32_DllName, SetLastError = true)]
+        public static extern bool EnableMenuItem(IntPtr hMenu, uint uIDEnableItem, uint uEnable);
+
+        [DllImport(User32_DllName, SetLastError = true)]
+        public static extern int TrackPopupMenuEx(IntPtr hMenu, uint uFlags, int x, int y, IntPtr hWnd, IntPtr lpTPMParams);
 
         [StructLayout(LayoutKind.Sequential)]
         public struct COPYDATASTRUCT
